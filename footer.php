227fae6d2c41299d450e976fe4d6baf4454ea77e
Kerrin McLaughlin <kerrin@expandtheroom.com>
2021-05-05 23:25:12 -0400

    footer links

--- footer.php
+++ footer.php
@@ -3,14 +3,16 @@
 ?>
 
 <footer class="fade-1 text-right text-fa my-12">
-<a class="text-fa no-underline" href=""><i class="text-fa fab fas fa-envelope-square"></i> </a>
-<a class="text-fa no-underline" href=""><i class="text-fa fab fa-twitter-square"></i> </a> 
-<a href="" class="no-underline text-fa"><i class="text-fa fab fa-linkedin"></i> </a>
-<a href="" class="no-underline text-fa"><i class="text-fa fab fa-dribbble-square"></i></a>
+    <a class="text-fa no-underline" href="mailto:hellokerrin@gmail.com"><i
+            class="text-fa fab fas fa-envelope-square"></i> </a>
+    <a class="text-fa no-underline" href="https://twitter.com/hello_kerrin"><i
+            class="text-fa fab fa-twitter-square"></i> </a>
+    <a href="https://www.linkedin.com/in/kerrinmclaughlin/" class="no-underline text-fa"><i
+            class="text-fa fab fa-linkedin"></i> </a>
 </footer>
 
 <script src="<?php echo get_template_directory_uri() ?>/assets/js/main2.js"></script>
 
 </body>
-  
-  </html>
+
+</html>
