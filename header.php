180512f497db2435976f4944b2d33da7cb6dc1f4
Kerrin McLaughlin <kerrin@expandtheroom.com>
2021-05-05 23:21:28 -0400

    favicon

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <title>Kerrin McLaughlin</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -9,20 +10,26 @@
     <script src="https://unpkg.com/scrollreveal"></script>
     <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/assets/imgs/unicorn.png"
+        type="image/x-icon">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 
-  </head>
-  <body style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/imgs/dot-bg.png');" class="mx-auto sm:w-4/5 w-3/5 lg:w-3/5 xl:w-1/2 text-black font-source font-medium leading-normal text-lg">
+</head>
+
+<body style="background-image: url('<?php echo get_template_directory_uri() ?>/assets/imgs/dot-bg.png');"
+    class="mx-auto sm:w-4/5 w-3/5 lg:w-3/5 xl:w-1/2 text-black font-source font-medium leading-normal text-lg">
     <header class="load-hidden">
-      <nav class="flex flex-wrap justify-between py-4">
-        <a class="no-underline " href="<?php echo get_home_url();?>"><span class="font-munro text-black text-2xl">kerrin mclaughlin</span></a>
-        <ul class="list-reset flex">
-          <li class="mr-6"><a href="<?php echo get_home_url();?>/category/work">work</a></li>
-          <li class="mr-6"><a href="<?php echo get_home_url();?>/category/writing">writing</a></li>
-          <li class="mr-6"><a href="<?php echo get_home_url();?>/contact">contact</a></li>
-        
-        </ul>
-      </nav>
-  
+        <nav class="flex flex-wrap justify-between py-4">
+            <a class="no-underline " href="<?php echo get_home_url();?>"><span
+                    class="font-munro text-black text-2xl">kerrin mclaughlin</span></a>
+            <ul class="list-reset flex">
+                <li class="mr-6"><a href="<?php echo get_home_url();?>/category/work">work</a></li>
+                <li class="mr-6"><a href="<?php echo get_home_url();?>/category/writing">writing</a></li>
+                <li class="mr-6"><a href="<?php echo get_home_url();?>/contact">contact</a></li>
+
+            </ul>
+        </nav>
+
     </header>
