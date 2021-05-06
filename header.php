23071d5b82942fdc105d56d7b98289ba243aeddb
Kerrin McLaughlin <kerrin@expandtheroom.com>
2021-05-05 23:22:47 -0400

    title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Kerrin McLaughlin</title>
+    <title>Kerrin McLaughlin - UX Designer and Researcher</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() ?>/assets/css/main.css">
