f8f16d5423401e0773339f21444ad8e1ad71dc63
Kerrin McLaughlin <kerrin@expandtheroom.com>
2021-05-05 23:12:59 -0400

    external writing

--- category.php
+++ category.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
-<h1 class="fade-1 w-full  pt-6 sm:text-4xl md:text-4xl tracking-wide font-munro load-hidden">all <?php single_cat_title();?></h1>
+<h1 class="fade-1 w-full  pt-6 sm:text-4xl md:text-4xl tracking-wide font-munro load-hidden">all
+    <?php single_cat_title();?></h1>
 <ul class=" fade-1 list-reset">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post();       
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post();       
   
   $title = get_the_title();
   $date = get_the_date();
@@ -12,24 +13,29 @@
 
   ?>
 
-<li class=" py-4">
-<?php if($thumbnail) { ?><img class="h-48 xl:h-64 w-full <?php echo $object_pos ?>" src="<?php echo $thumbnail ?>"><?php } ?><br>
+    <li class=" py-4">
+        <?php if($thumbnail) { ?><img class="h-48 xl:h-64 w-full <?php echo $object_pos ?>"
+            src="<?php echo $thumbnail ?>"><?php } ?><br>
 
-<?php if ($cat == " writing") { ?>
+        <?php if ($cat == " writing") { ?>
 
-<a href="<?php the_permalink(); ?>"><?php echo $title . $cat ?></a> <time class="pl-4 font-normal"> <?php echo  $date  ?> </time> 
-<?php }; ?>
+        <a href="<?php if($external) { echo $external;} else { the_permalink();} ?>"><?php echo $title . $cat ?></a>
+        <time class="pl-4 font-normal"> <?php echo  $date  ?> </time>
+        <?php }; ?>
 
-<?php if ($cat == " work") { ?>
+        <?php if ($cat == " work") { ?>
 
-  <h3 class="font-normal leading-normal pb-3"><?php echo get_post_meta($post->ID, "skills", true); ?> for <a href="<?php if($external) { echo $external;} else { the_permalink();} ?>"><?php echo $title ?></a></h3>
+        <h3 class="font-normal leading-normal pb-3"><?php echo get_post_meta($post->ID, "skills", true); ?> for <a
+                href="<?php if($external) { echo $external;} else { the_permalink();} ?>"><?php echo $title ?></a></h3>
 
-  <?php }; ?>
+        <?php }; ?>
 
 
-<p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-</li>
- <?php endwhile; endif; //ends the loop
+        <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.</p>
+    </li>
+    <?php endwhile; endif; //ends the loop
  ?>
- </ul>
+</ul>
 <?php get_footer(); ?>
